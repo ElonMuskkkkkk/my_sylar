45ac599c93f20b186d99a761a5f97902ef9c1962
ElonMuskkkkkk <yxxchange@163.com>
2023-04-10 11:43:44 +0800

    封装backtrace查看函数调用

--- sylar/inc/util.h
+++ sylar/inc/util.h
@@ -12,13 +12,31 @@
 #include <sys/types.h>
 #include <stdint.h>
 #include <cxxabi.h>
+#include <vector>
+#include <string>
+#include <execinfo.h>
 
 namespace sylar{
     pid_t GetThreadID();
     uint32_t GetFiberID();
+    /**
+     * @brief 获取当前的调用栈
+     * @param[out] bt 保存的调用栈
+     * @param[in] size 最多返回层数
+     * @param[in] skip 跳过栈顶的层数
+    */
+    void Backtrace(std::vector<std::string> &bt, int size = 64, int skip = 1);
+    /**
+     * @brief 获取当前栈信息的字符串
+     * @param[in] size 栈顶的最大层数
+     * @param[in] skip 跳过栈顶的层数
+     * @param[in] prefix 栈信息前输出的内容
+    */
+    std::string BacktraceToString(int size = 64, int skip = 2, const std::string &prefix = "");
 
     template <typename T>
-    const char* TypeToName(){
+    const char *TypeToName()
+    {
         static const char *s_name = abi::__cxa_demangle(typeid(T).name(), 0, 0, 0);
         return s_name;
     }
--- sylar/src/util.cpp
+++ sylar/src/util.cpp
@@ -1,7 +1,8 @@
 #include "../inc/util.h"
-
+#include "../inc/log.h"
 
 namespace sylar{
+    static Logger::ptr g_logger = SYLAR_LOG_NAME("system");
     pid_t GetThreadID()
     {
         return syscall(SYS_gettid);
@@ -10,5 +11,32 @@ namespace sylar{
     {
         //待完善
         return 2;
+    }
+    void Backtrace(std::vector<std::string> &bt, int size, int skip)
+    {
+        void **array = (void **)malloc(sizeof(void *) * size);
+        size_t s = backtrace(array, size);
+        char **strings = backtrace_symbols(array, s);
+        if(strings == nullptr){
+            SYLAR_LOG_ERROR(g_logger) << "backtrace synbols error";
+            return;
+        }
+        for (size_t i = skip; i < s;i++){
+            bt.push_back(strings[i]);
+        }
+        free(array);
+        free(strings);
+    }
+
+    std::string BacktraceToString(int size, int skip, const std::string &prefix)
+    {
+        std::vector<std::string> bt;
+        Backtrace(bt, size, skip);
+        std::stringstream ss;
+        for (size_t i = 0; i < bt.size(); i++)
+        {
+            ss << prefix << bt[i] << std::endl;
+        }
+        return ss.str();    
     }
 }
